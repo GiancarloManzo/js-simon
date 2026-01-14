dde37ec7a2a85513f271ae4d77659c59ceafd5e0
Giancarlo <cooper458.gm@gmail.com>
2026-01-14 16:40:37 +0100

    added function

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -14,3 +14,19 @@ const NUMBERS_COUNT = 5;
 let numbersToguess = [];
 let secondsLeft = TOTAL_SECONDS;
 let intervalId = null;
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateUniqueRandomNumbers(howMany, min, max) {
+  const numbers = [];
+  while (numbers.lenght < howMany) {
+    const n = getRandomInt(min, max);
+
+    if (!numbers.includes(n)) {
+      numbers.push(n);
+    }
+  }
+  return numbers;
+}
