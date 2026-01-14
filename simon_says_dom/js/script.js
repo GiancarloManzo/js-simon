64074cbe155efdc76025e6b749714951040f8902
Giancarlo <cooper458.gm@gmail.com>
2026-01-14 16:25:39 +0100

    added and fixed consts

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,2 +1,9 @@
 const countDownEl = document.getElementById("countdown");
-const inputs = document.getElementById("input");
+const instructionsEl = document.getElementById("instructions");
+const numbersListEl = document.getElementById("numbers-list");
+
+const answersForm = document.getElementById("answers-form");
+const inputGroup = document.getElementById("input-group");
+const messageEl = document.getElementById("message");
+
+const inputs = document.querySelectorAll("input");
